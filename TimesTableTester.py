4986af0b6aed9c17c02935ff9a6f8adb0ea72578
Eric Ly <102763906+eeeerrrriiiicccc@users.noreply.github.com>
2022-04-06 13:39:46 +1200

    Add files via upload
    Created basic gui

--- TimesTableTester.py
+++ TimesTableTester.py
@@ -1,10 +1,26 @@
 from tkinter import *
-from tkinter import ttk
 
-root = Tk()
-root.title("Times Tables")
+class TimesTables:
+    def __init__(self, parent):
 
-notebook = ttk.Notebook(root)
-notebook.pack(pady=150, padx=250)
+        f1 = Frame(parent)
 
-root.mainloop()
+        self.output_label = Label(f1, text="9 x 3 ")
+        self.output_label.grid(row=0, column=0)
+
+        self.q_entry = Entry(f1)
+        self.q_entry.grid(row=0, column=1)
+
+        self.check_Ans = Button(f1, text="Check Answer")
+        self.check_Ans.grid(row=1, column=0)
+
+        self.next = Button(f1, text="Next")
+        self.next.grid(row=1, column=1)
+
+        f1.pack()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    buttons = TimesTables(root)
+    root.mainloop()
